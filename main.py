2951f678278a2b1c37e09999b6c89a5076e720a3
skelouse <sam@skelouse.com>
2021-03-16 16:09:24 -0500

    built dropdown column selection

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import os
+import pandas as pd
 from sys import platform as sysplatform
 from kivy.app import App
 from kivy.uix.image import AsyncImage
 from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.modalview import ModalView
+from kivy.factory import Factory
+from kivy.properties import ListProperty, ObjectProperty
+from kivy.uix.dropdown import DropDown
 import matplotlib.pyplot as plt
 
 
@@ -22,8 +28,41 @@ class LoadPopup(ModalView):
 
     def __init__(self, **kwargs):
         super(LoadPopup, self).__init__(**kwargs)
+        self.app = App.get_running_app()
         self.size_hint = (.5, .3)
         self.build()
+        self.modals = {
+            'test': 0,
+            'storage': 0,
+            'url': 0
+        }
+
+    def get_modal(self, _type):
+        if _type == "test":
+            if self.modals[_type]:
+                return self.test_modal
+            else:
+                self.test_modal = ModalView()
+                self.test_grid = GridLayout(cols=1)
+                test_datasets = os.listdir("./test-data")
+                for _set in test_datasets:
+                    btn = Button(text=_set)
+                    btn.bind(on_press=self.load_data)
+                    self.test_grid.add_widget(btn)
+                self.test_modal.add_widget(self.test_grid)
+
+                self.modals[_type] = 1
+                return self.test_modal
+        elif _type == "storage":
+            pass
+        elif _type == "url":
+            pass
+
+    def load_data(self, event):
+        df = pd.read_csv("./test-data/%s" % str(event.text))
+        self.app.data = df
+        self.test_modal.dismiss()
+        self.dismiss()
 
     def build(self):
         self.load_grid = GridLayout(cols=1)
@@ -46,6 +85,8 @@ class LoadPopup(ModalView):
 
     def load_test_data(self, event):
         print("Loading data from app storage")
+        self.get_modal("test").open()
+
 
     def load_from_storage(self, event):
         print("Loading data from phone storage")
@@ -58,6 +99,59 @@ class TestApp(App):
     def __init__(self, **kwargs):
         super(TestApp, self).__init__(**kwargs)
         self.load_popup = LoadPopup()
+        self.load_popup.bind(on_dismiss=self.load_data)
+        self.data = None
+        self.x_dropdown = None
+        self.y_dropdown = None
+        self.x_cols = []
+        self.y_cols = []
+
+    def load_data(self, event):
+        print(self.data.head())
+        cols = self.data.columns
+        self.x_cols = []
+        if self.x_dropdown:
+            self.x_btn.unbind(on_release=self.x_dropdown.open)
+        del self.x_dropdown
+        self.x_dropdown = DropDown(dismiss_on_select=False)
+        for col in cols:
+            btn = ToggleButton(
+                text=col,
+                size_hint_y=None,
+                height=44
+            )
+            btn.bind(on_release=lambda btn: self.x_dropdown.select(btn.text))
+            self.x_dropdown.add_widget(btn)
+        self.x_btn.bind(on_release=self.x_dropdown.open)
+        self.x_dropdown.bind(on_select=self.select_xcol)
+
+        self.y_cols = []
+        if self.y_dropdown:
+            self.y_btn.unbind(on_release=self.y_dropdown.open)
+        del self.y_dropdown
+        self.y_dropdown = DropDown(dismiss_on_select=False)
+        for col in cols:
+            btn = ToggleButton(
+                text=col,
+                size_hint_y=None,
+                height=44
+            )
+            btn.bind(on_release=lambda btn: self.y_dropdown.select(btn.text))
+            self.y_dropdown.add_widget(btn)
+        self.y_btn.bind(on_release=self.y_dropdown.open)
+        self.y_dropdown.bind(on_select=self.select_ycol)
+
+    def select_xcol(self, event, col):
+        if col in self.x_cols:
+            self.x_cols.remove(col)
+        else:
+            self.x_cols.append(col)
+
+    def select_ycol(self, event, col):
+        if col in self.y_cols:
+            self.y_cols.remove(col)
+        else:
+            self.y_cols.append(col)
 
     def build(self):
         fig, ax = plt.subplots(figsize=(8, 3.9))
@@ -80,10 +174,18 @@ class TestApp(App):
         self.top_grid.add_widget(load_btn)
 
         # Button for setting x column
+        self.x_btn = Button(text="x column")
+        self.top_grid.add_widget(self.x_btn)
 
         # Button for setting y column multi-line selection
+        self.y_btn = Button(text="y column")
+        self.top_grid.add_widget(self.y_btn)
 
-        # Button for 
+        # Button for plotting
+
+        self.plot_btn = Button(text="Plot it")
+        self.plot_btn.bind(on_press=self.plot)
+        self.top_grid.add_widget(self.plot_btn)
 
         # Button for
 
@@ -115,10 +217,17 @@ class TestApp(App):
         return layout
 
     def load(self, event):
+        if self.x_dropdown:
+            self.x_dropdown.dismiss()
+        if self.y_dropdown:
+            self.y_dropdown.dismiss()
         self.load_popup.open()
 
-    def callback(self, event):
-        plt.plot([1, 2, 3], [5, 9, 12])
+    def plot(self, event):
+        X = self.data[self.x_cols]
+        y = self.data[self.y_cols]
+        plt.plot(X, y)
+        plt.legend()
         plt.savefig(self.filename)
         plt.clf()  # cla for multiple subplots
         self.img.source = self.filename
